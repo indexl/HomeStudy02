340cb161a90bbca5154689bdac352e3bba20fc33
indexl <indexl@naver.com>
2024-11-04 21:49:06 +0900

    Di

---
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+
+	@GetMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+
+	@GetMapping("/usr/home/main2")
+	@ResponseBody
+	public int showMain2() {
+		return 10;
+	}
+}
